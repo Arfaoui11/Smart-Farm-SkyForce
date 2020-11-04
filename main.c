288b4ba9545d5b6b7bcd74e2000a2f5c5aabe0bb
med <mohamedamine.benothmane@esprit.tn>
2020-11-04 23:03:58 +0100

    ajout

--- main.c
+++ main.c
@@ -1,16 +1,8 @@
-#include <stdio.h>
-#include "fonctions.h"
-int main() {
-int a;
-int b;
-printf("\nSaisie du Premier Nombre: ");
-scanf("%d", &a);
-do {
-printf("\nsaisie du Second Nombre: ");
-scanf("%d", &b);
-} while(b == 0);
-printf("\n%d + %d= %d", a, b, sommer(a,b));
-printf("\n%d - %d= %d", a, b, soustraire(a,b));
-printf("\n%d / %d = %f\n", a, b, diviser(a,b));
-return (0);
-}
+#include<stdio.h>
+int main()
+{
+int a,b;
+a=10;
+b=3;
+printf("\ a=%d b=%d",a,b);
+} 

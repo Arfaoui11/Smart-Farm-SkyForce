5a1c1e159b2632eca8eff9bd8fca80c98d047a5d
arfaoui11 <mahdi.arfaoui1@esprit.tn>
2020-11-04 06:15:36 -0800

    cc

--- main.c
+++ main.c
@@ -1,9 +1,16 @@
-#include<stdio.h>
-void main ()
-{
-int a ,b , c;
-a=10;
-b=3;
-c=a+b;
-printf("%d+%d=%d",a,b,c);
+#include <stdio.h>
+#include "fonctions.h"
+int main() {
+int a;
+int b;
+printf("\nSaisie du Premier Nombre: ");
+scanf("%d", &a);
+do {
+printf("\nsaisie du Second Nombre: ");
+scanf("%d", &b);
+} while(b == 0);
+printf("\n%d + %d= %d", a, b, sommer(a,b));
+printf("\n%d - %d= %d", a, b, soustraire(a,b));
+printf("\n%d / %d = %f\n", a, b, diviser(a,b));
+return (0);
 }
